82c817a47c5adf5950a8120976c018a8b83c865e
hash# <harshkhavale1102@gmail.com>
2023-02-04 19:24:38 +0530

    Update PaymentPage.aspx.cs

--- WebPages/PaymentPage.aspx.cs
+++ WebPages/PaymentPage.aspx.cs
@@ -197,7 +197,7 @@ public partial class WebPages_PaymentPage : System.Web.UI.Page
         // email send process -
         String ToEmailAddress = Email;
         String Username = UserName;
-        MailMessage PassRecMail = new MailMessage("variables.pvt.lmt@gmail.com", ToEmailAddress);
+        MailMessage PassRecMail = new MailMessage("WEBSITE_EMAIL", ToEmailAddress);
         PassRecMail.Body = EmailBody;
         PassRecMail.IsBodyHtml = true;
         PassRecMail.Subject = "Order Approved!";
@@ -206,7 +206,7 @@ public partial class WebPages_PaymentPage : System.Web.UI.Page
         {
             client.EnableSsl = true;
             client.UseDefaultCredentials = false;
-            client.Credentials = new NetworkCredential("variables.pvt.lmt@gmail.com", "fmzrswkdeymytrpr");
+            client.Credentials = new NetworkCredential("WEBSITE_EMAIL", "APP_CODE");
             client.Host = "smtp.gmail.com";
             client.Port = 587;
             client.DeliveryMethod = SmtpDeliveryMethod.Network;
